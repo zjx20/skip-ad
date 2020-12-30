0b3e00fef9ac6af6954eae7f6178c65504464dff
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-12-29 22:05:30 -0300

    Skip ads that have no skip button
    Based on https://stackoverflow.com/a/48071848

--- youtube.js
+++ youtube.js
@@ -2,4 +2,12 @@ setInterval(() => {
   for (const button of document.getElementsByClassName("ytp-ad-skip-button")) {
     button.click(); // "Skip Ad" or "Skip Ads" buttons
   }
+
+  let progressBar = document.getElementsByClassName('ytp-play-progress')[0];
+  if (getComputedStyle(progressBar)['background-color'] === 'rgb(255, 204, 0)') {
+    let video = document.getElementsByClassName('html5-main-video')[0];
+    if (!video.paused) {
+      video.currentTime = video.duration;
+    }
+  }
 }, 300);
